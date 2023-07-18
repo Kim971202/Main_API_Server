79baea41df4f52950080a4be1d0d7cf099161adb
Kim971202 <kimyohan971202@gmail.com>
2023-07-18 12:04:51 +0900

    env 설정 파일 추가

--- DB/pool.js
+++ DB/pool.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "1.226.248.99",
-  port: "3306",
-  user: "ento",
-  password: "gnHo0vjkgVXCiJr",
-  database: "hnintegratedserver",
-  connectionLimit: 50,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  connectionLimit: process.env.MYSQL_LIMIT,
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 const port = 3000; // 서버 포트번호
 
